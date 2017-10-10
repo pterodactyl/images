c603e1746534799082a2c55234eceb4c840332dd
Dane Everitt <dane@daneeveritt.com>
2017-10-09 23:10:54 -0500

    Add support for accessing internal IP
    Pterodactyl/Panel#613

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/ash
-sleep 5
+sleep 2
 
 cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
