79d9c640383d8abc060f86489f90f22824ed3b45
Unknown <isaacalich8401@gmail.com>
2017-11-05 21:42:29 -0500

    Allow Source autoupdating

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,11 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+# Update Source Server
+if [ -z ${SRCDS_APPID} ]; then
+    ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
