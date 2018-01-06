c76f7e41f6ad7d047c96eed4ff374ca9917d6522
Dane Everitt <dane@daneeveritt.com>
2018-01-06 15:15:22 -0600

    Remove sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sleep 2
-
 cd /home/container
 
 # Make internal Docker IP address available to processes.
@@ -16,4 +14,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
