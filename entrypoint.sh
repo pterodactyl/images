9bb8ea32b84834b4abfd651c67a7e09a172b1ff1
Dane Everitt <dane@daneeveritt.com>
2018-01-06 14:54:47 -0600

    Cleaner output when running container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sleep 2
-
 cd /home/container
 
 # Output Current Java Version
@@ -14,9 +12,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
-
-if [ $? -ne 0 ]; then
-    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
-    exit 1
-fi
+eval ${MODIFIED_STARTUP}
