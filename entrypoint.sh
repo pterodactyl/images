f8938646888ab15c0f7c144ac4e58539c1603eb4
Dane Everitt <dane@daneeveritt.com>
2018-01-06 14:55:52 -0600

    cleaner output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/ash
-sleep 2
-
 cd /home/container
 
 # Make internal Docker IP address available to processes.
@@ -11,9 +9,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
-
-if [ $? -ne 0 ]; then
-    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
-    exit 1
-fi
+eval ${MODIFIED_STARTUP}
