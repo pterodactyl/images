c705b6fc61e28df5aab87e8790b086ee01d535ae
Dane Everitt <dane@daneeveritt.com>
2018-01-06 14:55:21 -0600

    cleaner output

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,9 +14,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
-
-if [ $? -ne 0 ]; then
-    echo "PTDL_CONTAINER_ERR: There was an error while attempting to run the start command."
-    exit 1
-fi
+eval ${MODIFIED_STARTUP}
