6f9613743ff94936c41b00c10d25bdcde3da0ec0
Isaac A <isaacalich8401@gmail.com>
2018-09-10 17:04:19 -0400

    It's called uMod now apparently...

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,11 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 if [ -f OXIDE_FLAG ] || [ "${OXIDE}" = 1 ]; then
-    echo "Updating OxideMod..."
-    curl -sSL "https://github.com/OxideMod/Oxide/releases/download/latest/Oxide-Rust.zip" > oxide.zip
+    echo "Updating uMod..."
+    curl -sSL "https://umod.org/games/rust/download" > oxide.zip
     unzip -o -q oxide.zip
     rm oxide.zip
-    echo "Done updating OxideMod!"
+    echo "Done updating uMod!"
 fi
 
 # Fix for Rust not starting
