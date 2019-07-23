fbd0925922cc7d77922423f721ea226417ad3f00
Michael (Parker) Parker <parkervcp@gmail.com>
2019-07-23 13:44:33 -0400

    update source image with auto-update options
    added variable that can be set to 0 to disable auto_update.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,25 @@ sleep 1
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Update Source Server
-if [ ! -z ${SRCDS_APPID} ]; then
-    if [ ! -z ${HLDS_GAME} ]; then
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +app_set_config 90 mod ${HLDS_GAME} +quit
-    elif [ ! -z ${SRCDS_BETAID} ]; then
-        if [ ! -z ${SRCDS_BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+
+## if auto_update is no set or to 1 update
+if [  -z ${AUTO_UPDATE} ] || [ ${AUTO_UPDATE} == 1 ]; then 
+    # Update Source Server
+    if [ ! -z ${SRCDS_APPID} ]; then
+        if [ ! -z ${HLDS_GAME} ]; then
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +app_set_config 90 mod ${HLDS_GAME} +quit
+        elif [ ! -z ${SRCDS_BETAID} ]; then
+            if [ ! -z ${SRCDS_BETAPASS} ]; then
+                ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+            else
+                ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+            fi
         else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
         fi
-    else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
     fi
+else
+    echo -e "not updating game server as auto update was set to 0"
 fi
 
 # Replace Startup Variables
