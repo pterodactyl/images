593cf85f84a34b09ef908cc0a4ca305888edfa73
Purpur <43528123+Pemigrade@users.noreply.github.com>
2021-02-05 19:22:26 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ java -version
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo '${STARTUP}' | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
